e4fa84e224d62c67c7a845b90b688b35e18fa529
shibadog <shiba_dog@outlook.com>
2023-06-22 13:04:39 +0900

    init commit

---
+++ src/main/java/jp/sbps/kentaimai01/sample/log4j2logrotate/SampleLog4j2LogrotateApplication.java
@@ -0,0 +1,13 @@
+package jp.sbps.kentaimai01.sample.log4j2logrotate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleLog4j2LogrotateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleLog4j2LogrotateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/sbps/kentaimai01/sample/log4j2logrotate/SampleLog4j2LogrotateApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.sbps.kentaimai01.sample.log4j2logrotate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleLog4j2LogrotateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
